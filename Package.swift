48c9ba549b0d3aaf8a31a17c7095d0bb954479a8
Wolf McNally <wolf@wolfmcnally.com>
2020-01-22 20:41:40 -0800

    Added platforms and dependencies.

--- Package.swift
+++ Package.swift
@@ -5,15 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "WolfQRCodes",
+    platforms: [
+        .iOS(.v12), .macOS(.v10_13), .tvOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "WolfQRCodes",
+            type: .dynamic,
             targets: ["WolfQRViews"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfWith", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfPipe", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfFoundation", from: "5.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfImage", from: "4.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfViews", from: "5.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfConcurrency", from: "3.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfNesting", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfApp", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfAutolayout", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
